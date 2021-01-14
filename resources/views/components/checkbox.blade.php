70491f3699d38b7e37b6b501c2a1594319c68a67
Isaac Sai <isaacsai030@gmail.com>
2021-01-14 22:02:58 +0000

    Ammend checkbox to default behaviour

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,8 +1,8 @@
-@props(['name', 'title', 'value' => null, 'id' => null, 'subtitle' => null, 'valueName' => null, 'checked' => false])
+@props(['name', 'title', 'value' => 'on', 'id' => null, 'subtitle' => null, 'valueName' => null, 'checked' => false])
 
 <div class="flex space-x-3 {{ $subtitle == null ? 'items-center' : 'items-start' }}">
     <div>
-        <input type="checkbox" id="{{ $id ?? $name }}" name="{{ $name }}" @if($value) value="{{ $value }}" @endif @if((is_array(old($valueName ?? $name)) ? in_array($value, old($valueName ?? $name)) : ($value != null && $value == old($valueName ?? $name))) || $checked) checked @endif {{ $attributes->merge(['class' => 'rounded border-gray-300 text-primary-600 focus:ring-primary-500']) }}>
+        <input type="checkbox" id="{{ $id ?? $name }}" name="{{ $name }}" @if($value) value="{{ $value }}" @endif @if((is_array(old($valueName ?? $name)) ? in_array($value, old($valueName ?? $name)) : $value == old($valueName ?? $name)) || $checked) checked @endif {{ $attributes->merge(['class' => 'rounded border-gray-300 text-primary-600 focus:ring-primary-500']) }}>
     </div>
     <div class="text-sm">
         <label class="text-gray-700 font-medium tracking-wide" for="{{ $id ?? $name }}">{{ $title }}</label>
